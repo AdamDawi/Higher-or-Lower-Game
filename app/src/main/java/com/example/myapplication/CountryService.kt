7d14073bfe76ef6842530c32e078fa4a6a8b31e1
Adam Dawidziuk <adamdawidziuk@onet.pl>
2023-10-22 16:16:02 +0200

    progress dialog, draw country to choose from

--- app/src/main/java/com/example/myapplication/CountryService.kt
+++ app/src/main/java/com/example/myapplication/CountryService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 
 interface CountryService
 {
-    @GET("v3.1/all")
+    @GET("v3.1/independent?status=true")
 
     fun getCountry() : Call<List<CountryModel>>
 }
--- app/src/main/java/com/example/myapplication/GameActivity.kt
+++ app/src/main/java/com/example/myapplication/GameActivity.kt
@@ -1,8 +1,13 @@
 package com.example.myapplication
 
+import android.app.Dialog
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.ActivityGameBinding
@@ -11,15 +16,20 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity()
 {
     private var binding: ActivityGameBinding? = null
+    private var countryList: List<CountryModel>? = null
+    private var customProgressDialog: Dialog? = null
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        showProgressDialog()
 
         getCountriesDataDetails()
     }
@@ -44,16 +54,14 @@ class GameActivity : AppCompatActivity()
             ) {
                 if(response.isSuccessful)
                 {
-                    val countryList: List<CountryModel>? = response.body()
+                    cancelProgressDialog()
+                    changeGraphicsElementsVisibility()
+                    countryList = response.body()
                     Log.i("Response RESULT: ", "$countryList")
                     Log.i("Response SIZE: ", countryList?.size.toString())
 
-                    //Glide for load images from link to xml element
-                    Glide.with(this@GameActivity).load(countryList!![60].flags.png).into(binding!!.ivCircleImageUp)
-                    Glide.with(this@GameActivity).load(countryList[40].flags.png).into(binding!!.ivCircleImageDown)
-                    binding?.tvNameUp?.text = countryList!![60].name.common
-                    binding?.tvNameDown?.text = countryList!![40].name.common
-                    //.override(300, 300).apply(RequestOptions().transform(CircleCrop()))
+                    setCountryData(countryList?.get(getRandomNumber(countryList?.size))!!, 0)
+                    setCountryData(countryList!![getRandomNumber(countryList?.size)], 1)
                 }
                 else
                 {
@@ -71,8 +79,72 @@ class GameActivity : AppCompatActivity()
         })
     }
 
+    private fun setCountryData(country: CountryModel, countryDataType: Int)
+    {
+        //0 is upper country
+        //1 is bottom country
+        if(countryDataType==0)
+        {
+            //Glide for load images from link to xml element
+            Glide.with(this@GameActivity).load(country.flags.png).into(binding!!.ivCircleImageUp)
+            binding?.tvNameUp?.text = country.name.common
+            binding?.tvPopulationNumberUp?.text = addCommasToPopulationData(country.population)
+        }else if(countryDataType==1)
+        {
+            //Glide for load images from link to xml element
+            Glide.with(this@GameActivity).load(country.flags.png).into(binding!!.ivCircleImageDown)
+            binding?.tvNameDown?.text = country.name.common
+            Log.e("Test", country.population)
+            binding?.tvPopulationNumberDown?.text = addCommasToPopulationData(country.population)
+        }
 
+        //.override(300, 300).apply(RequestOptions().transform(CircleCrop()))
+    }
 
+    private fun addCommasToPopulationData(population: String): String
+    {
+        val length = population.length
+        val stringBuilder = StringBuilder()
+
+        for (i in 0 until length)
+        {
+            stringBuilder.append(population[i])
+            if (i < length - 1 && (i + 1) % 3 == length % 3)
+            {
+                stringBuilder.append(',')
+            }
+        }
+        return stringBuilder.toString()
+    }
+
+    private fun showProgressDialog()
+    {
+        customProgressDialog = Dialog(this@GameActivity)
+        customProgressDialog?.setContentView(R.layout.custom_progress_dialog)
+        customProgressDialog?.setCancelable(true)
+        customProgressDialog?.show()
+    }
+
+    private fun cancelProgressDialog()
+    {
+        customProgressDialog?.dismiss()
+    }
+
+    private fun changeGraphicsElementsVisibility()
+    {
+        //changing the visibility of elements because they are invisible at the start
+        binding?.tvPopulationUp?.visibility = View.VISIBLE
+        binding?.tvPopulationDown?.visibility = View.VISIBLE
+        binding?.flOR?.visibility = View.VISIBLE
+    }
+
+    private fun getRandomNumber(borderNumber: Int?): Int
+    {
+        // Create class object
+        val random = Random(System.currentTimeMillis())
+        // Generate random number from 0 to borderNumber
+        return random.nextInt(0, borderNumber!!)
+    }
 
     override fun onDestroy()
     {
